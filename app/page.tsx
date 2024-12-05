4f466f20b20739db508a711267fbba7639275e9e
Jack Silverio <silveriocieza.jackanibal@gmail.com>
2024-12-05 17:28:01 -0500

    fixing

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import dynamic from "next/dynamic";
 import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
@@ -6,18 +5,14 @@ import Grid from "@/components/Grid";
 import Presentation from "@/components/Presentation";
 import RecentProjects from "@/components/RecentProjects";
 import Skills from "@/components/Skills";
+// import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
-
-const FloatingNav = dynamic(
-  () => import("@/components/ui/FloatingNav").then((mod) => mod.FloatingNav),
-  { ssr: false }
-);
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
+        {/* <FloatingNav navItems={navItems} /> */}
         <Presentation />
         <Grid />
         <RecentProjects />
